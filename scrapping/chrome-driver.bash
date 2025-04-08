e891afc18d13116b5c3e220638fc549837d1e31a
Zensi77 <juanmaespi7@gmail.com>
2025-04-08 22:17:27 +0200

    Modificacion de la arquitectura y desarrollo del script para scraping

--- scrapping/chrome-driver.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Instalar chromium y chromedriver
-apt-get update
-apt-get install -y chromium-browser chromium-chromedriver
--- scrapping/scrap.py
+++ scrapping/scrap.py
@@ -9,11 +9,17 @@ from selenium.webdriver.chrome.options import Options
 import csv
 
 # Página de inicio de ambas fuentes a recolectar información
-fonts = ['https://www.amazon.com', 'https://www.pccomponentes.com']
+fonts = ['https://www.amazon.com/', 'https://www.coolmod.com/']
 
 # Ids del campo de búsqueda
-inputSearch = ['twotabsearchtextbox', 'search']
+inputSearch = ['twotabsearchtextbox', 'seek']
 searchTerms = ['monitor gaming 27 pulgadas FHD', 'Ordenador sobremesa Gaming', 'Smartphone 5G', 'Portátil Gaming', 'Auriculares gaming', 'ratón gaming', 'teclado gaming', 'monitor gaming 27 pulgadas 4K', 'monitor gaming 27 pulgadas 2K']
+cardProductClass = ['a-link-normal s-line-clamp-4 s-link-style a-text-normal', 'dfd-card-link']
+
+# Informacion de los articulos a extraer
+info = ['Nombre', 'Precio', 'Enlace', 'Imagen', 'Descripción', 'Valoraciones']
+
+
 
 options = Options()
 options.add_argument('--headless')  # Ejecutar en segundo plano
@@ -23,26 +29,51 @@ options.add_argument('--disable-dev-shm-usage') # Desactivar el uso de memoria c
 # Usar el ChromeDriver con WebDriverManager
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-for font, input_search in zip(fonts, inputSearch): # Iterar sobre las fuentes y sus respectivos campos de búsqueda
+for font, input_search, products in zip(fonts, inputSearch, cardProductClass): # Iterar sobre las fuentes y sus respectivos campos de búsqueda
     driver.get(font)
     try:
-        # Buscar el campo de búsqueda
-        search_box = WebDriverWait(driver, 10).until(
+        # Buscar el campo de búsqueda, esperando a que esté presente
+        search_box = WebDriverWait(driver, 30).until(
             EC.presence_of_element_located((By.ID, input_search))
         )
-        
-        # Enviar los términos de búsqueda
-        search_box.send_keys(searchTerms[0])
-        
-        # Esperar a que la página cargue
-        page = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, 'body'))
-        )
+
+        for term in searchTerms:
+            # Limpiar el campo de búsqueda antes de enviar un nuevo término
+            search_box.clear()
+
+            # Enviar los términos de búsqueda
+            search_box.send_keys(searchTerms[0])
+
+            # Esperar a que la página cargue
+            page = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, 'body'))
+            )
+
+            # Obtener los 10 primeros resultados de búsqueda
+            results = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, products))
+            )
+
+            for item in results:
+                #navego al href del producto
+                item.click()
+
+                # Esperar a que la página cargue
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.TAG_NAME, 'body'))
+                )
+
+            # Guardar los resultados en un archivo CSV
+
         
         # Aquí puedes agregar el código para extraer la información relevante.
         # Ejemplo: buscar productos, extraer nombres, precios, etc.
         
+
+        print(f"Página {font} cargada correctamente.")
     except Exception as e:
-        print(f"Error en {font}: {e}")
-    finally:
-        driver.quit()
+        print(f"Error en al leer la página {font}")
+        import traceback
+        traceback.print_exc()
+
+driver.quit()
